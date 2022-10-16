f593886c65a0c1a11b100d1c4855ace128fcaea3
Victhereum <tamsvictorisaac@gmail.com>
2022-10-16 17:12:50 +0100

    fixed migration issue

--- equestlms/home/migrations/0002_alter_customuser_profile_pic.py
+++ equestlms/home/migrations/0002_alter_customuser_profile_pic.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='customuser',
             name='profile_pic',
-            field=django_resized.forms.ResizedImageField(blank=True, crop=None, force_format=None, keep_meta=True, null=True, quality=-1, scale=None, size=[1920, 1080], upload_to=equestlms.utils.media.MediaHelper.get_image_upload_path, verbose_name='Profile Picture'),
+            field=django_resized.forms.ResizedImageField(blank=True, crop=None, force_format=None, keep_meta=True, null=True, quality=-1, size=[1920, 1080], upload_to=equestlms.utils.media.MediaHelper.get_image_upload_path, verbose_name='Profile Picture'),
         ),
     ]
