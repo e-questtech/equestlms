951d76d15e44be58e5340b30f110467cfb092cfd
officialksolomon <solomonuche42@gmail.com>
2022-10-17 20:05:25 +0100

    updated pagination and video iframe...

--- videos/views.py
+++ videos/views.py
@@ -1,12 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from videos.models import Video
 
 
-class VideoListView(TemplateView):
+class VideoListView(ListView):
+    model = Video
+    context_object_name: str = 'video_list'
     template_name: str = 'videos/video_list.html'
+    paginate_by = 1
+    queryset = Video.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['video_list'] = Video.objects.all()
         context['latest_videos'] = Video.objects.all().order_by('-created_at')[:3]
         return context
