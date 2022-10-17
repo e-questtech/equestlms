c25d80558acd749c8e8fab0f8f6bbebcbe665a6f
officialksolomon <solomonuche42@gmail.com>
2022-10-17 16:07:11 +0100

    added video_list and lastest_post template logic

--- videos/views.py
+++ videos/views.py
@@ -1,4 +1,12 @@
 from django.views.generic import TemplateView
+from videos.models import Video
+
 
 class VideoListView(TemplateView):
-  template_name: str = 'videos/video_list.html'
+    template_name: str = 'videos/video_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['video_list'] = Video.objects.all()
+        context['latest_videos'] = Video.objects.all().order_by('-created_at')[:3]
+        return context
