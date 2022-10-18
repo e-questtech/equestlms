bcbab1addd6c099ee48a0cabf9950d0fab37f294
Gentility01 <mastergentility5@gmail.com>
2022-10-18 15:47:09 +0100

    ticket 22 worked

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from ckeditor.fields import RichTextField
 from django.db import models
 from django.utils.text import slugify
+from django_resized import ResizedImageField
 
 from equestlms.home.models import CustomUser
 from equestlms.utils.models import TimeBasedModel
@@ -8,6 +9,15 @@ from equestlms.utils.models import TimeBasedModel
 
 class Category(TimeBasedModel):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=125, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
@@ -19,14 +29,18 @@ class Tags(TimeBasedModel):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
 
 class Blog(TimeBasedModel):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=125, blank=True, null=True)
-    tags = models.ManyToManyField(Tags, null=True)
+    tags = models.ManyToManyField(Tags)
     description = RichTextField()
-    image = models.ImageField(upload_to="blog_img/")
+    image = ResizedImageField(size=[400, 300], upload_to="blog_img/")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
@@ -35,3 +49,7 @@ class Blog(TimeBasedModel):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Blog"
+        verbose_name_plural = "Blogs"
--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,10 @@ app_name = "blog"
 urlpatterns = [
     path("", view=views.BlogListView.as_view(), name="blog_list"),
     path("<slug:slug>", view=views.BlogDetailView.as_view(), name="blog_detail"),
+    path(
+        "category/<str:slug>/",
+        view=views.CategoryListView.as_view(),
+        name="category_detail",
+    ),
+    path("search/", view=views.SearchListView.as_view(), name="search_view"),
 ]
