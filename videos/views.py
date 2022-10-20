62842bf8eebf3f88d828f544ba819d0de1286124
Victhereum <tamsvictorisaac@gmail.com>
2022-10-20 04:41:29 +0100

    Fixed video url

--- videos/views.py
+++ videos/views.py
@@ -1,15 +1,16 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView
+
 from videos.models import Video
 
 
 class VideoListView(ListView):
     model = Video
-    context_object_name: str = 'video_list'
-    template_name: str = 'videos/video_list.html'
-    paginate_by = 1
+    context_object_name: str = "video_list"
+    template_name: str = "videos/video_list.html"
+    paginate_by = 3
     queryset = Video.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_videos'] = Video.objects.all().order_by('-created_at')[:3]
+        context["latest_videos"] = Video.objects.all().order_by("-created_at")[:3]
         return context
