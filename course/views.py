8af1edbea35b6adc0a26c6ed6725d92599e1c74c
officialksolomon <solomonuche42@gmail.com>
2022-10-23 07:10:19 +0100

    Added mailing  admin and displaying message to user feature on purchase.

--- course/views.py
+++ course/views.py
@@ -1,6 +1,12 @@
+from django.shortcuts import redirect
 from django.views.generic import DetailView, ListView
+from django.views import View
+from .models import Course
+from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+User = get_user_model()
 
-from course.models import Course
 
 class AvailableCourseListView(ListView):
     model = Course
@@ -17,3 +23,29 @@ class CourseDetailView(DetailView):
     model = Course
     template_name = "course/course-details.html"
     context_object_name = "course"
+
+
+class HandlePurchaseView(View):
+    """
+    Sends user details to admin admin and displays purchase status to user
+    """
+
+    def get(self, request, pk):
+        course = Course.objects.get(pk=pk)
+        if request.user.is_authenticated:
+            user = User.objects.get(pk=request.user.pk)
+            # Email admin
+            admin_email_details = {
+                'subject': 'COURSE PURCHASE NOTIFICATION',
+                'message': f'A user with name {user} made a  purchase request. \n User details are as follows: \n Email: {user.email}',  # type: ignore
+                'recipient_list': ['solomonuche42@gmail.com'],
+                'from_email': 'equestlms@equestlms.com'
+
+            }
+            send_mail(subject=admin_email_details['subject'], message=admin_email_details["message"],
+                      from_email=admin_email_details['from_email'],  recipient_list=admin_email_details['recipient_list'])  # type: ignore
+            # display message to user
+            messages.add_message(request, messages.INFO,
+                                 'Thank you for learning with Equest, your purchase request has been sent.')
+
+        return redirect(course)
