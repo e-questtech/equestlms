f9c89d54c727cb5aca5fbe783f073151c98fea8e
Victhereum <tamsvictorisaac@gmail.com>
2023-03-23 16:50:23 +0100

    Setting up for render

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,7 +9,9 @@ from .base import MIDDLEWARE, ROOT_DIR, env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["equesttech.herokuapp.com"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS", default=["https://equest-lms.onrender.com"]
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
